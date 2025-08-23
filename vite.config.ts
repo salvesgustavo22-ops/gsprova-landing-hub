558423240d8a32c0a591eab4a06dd3018f5478f9
salvesgustavo22-ops <salvesgustavo22@gmail.com>
2025-08-23 18:06:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/gsprova-landing-hub/', // 👈 caminho do repositório no GitHub Pages
 })
