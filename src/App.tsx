9d12af38fb8ec7733400ca8d96e5f75792b97078
salvesgustavo22-ops <salvesgustavo22@gmail.com>
2025-08-23 18:18:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+// import { Toaster } from '@/components/ui/toaster'
+...
+{/* <Toaster /> */}
 
 const queryClient = new QueryClient();
 
