ce95799b0162cc7f1dd01e6ccab7f7a64d943e6a
Gustavo Alves <salvesgustavo22@gmail.com>
2025-09-07 23:45:02 -0300

    Update 20250904032556_9afbb8d2-6934-47a7-9636-c8e5ec5c75f3.sql

--- supabase/migrations/20250904032556_9afbb8d2-6934-47a7-9636-c8e5ec5c75f3.sql
+++ supabase/migrations/20250904032556_9afbb8d2-6934-47a7-9636-c8e5ec5c75f3.sql
@@ -1,4 +1,6 @@
 -- Allow only users with the 'editor' role to upload correction files
+SET QUOTED_IDENTIFIER ON;
+-- Allow only users with the 'editor' role to upload correction files
 CREATE POLICY "Editor uploads for corrections"
 ON storage.objects
 FOR INSERT
